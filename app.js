7044ff4f92ff1ebe7325dc78c30d7c0d2f9c18ca
eugeniab8 <euge_304@hotmail.com>
2023-09-21 17:14:53 -0300

    Agregué el login

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/login.html"));
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
 app.use(express.static('public'));
 
 app.listen(port, () => {
