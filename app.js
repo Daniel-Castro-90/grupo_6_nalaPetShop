c8eec50e30c4ff7f827f766463fbfb15812858a7
Daniel <castrodanieladr@gmail.com>
2023-10-21 17:48:55 -0300

    updating

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ app.use('/products', routeProducts);
 
 app.use('/users', routeUsers);
 
-//app.use((req, res, next) => {
-    //res.status(404).render('partials/not-found')
-//})
+app.use((req, res, next) => {
+    res.status(404).render('partials/not-found')
+})
 
 //server start
 app.listen(port, () => {
