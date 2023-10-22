0d2daa21fd39c7868c6cc5ee0839a9c8c3315e7f
camimena <lmgb.camila.mena@gmail.com>
2023-10-22 14:30:10 -0300

    adding new login form functio

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -16,10 +16,46 @@ const usersController = {
     },
     create: (req, res) =>{
         const users = getUsers();
+        let validator = validationResult(req);
+        const {email, password, confirmPassword, dni, tel} = req.body;
+        if (!email) {
+            return res.status(400).json({ mensaje: "Por favor completar todos los campos. Falta el email."});
+        }
+        if (!password) {
+            return res.status(400).json({ mensaje: "Por favor completar todos los campos. Falta la contraseña."});
+        }
+        if (!confirmPassword) {
+            return res.status(400).json({ mensaje: "Por favor completar todos los campos. Falta repetir la contraseña."})
+        }
+        if (!dni) {
+            return res.status(400).json({ mensaje: "Por favor completar todos los campos. Falta el DNI."});
+        }
+        if (!tel) {
+            return res.status(400).json({ mensaje: "Por favor completar todos los campos. Falta el teléfono."});
+        }
+        if(password !== confirmPassword){
+            return res.status(400).json({mensaje: "Por favor verificar las contraseñas. Deben coincidir exactamente."})
+        }
+
+        let userRegister = users.find(user => user.email === email);
+
+        if(userRegister){
+            return res.status(400).json({mensaje: "Usuario ya existente."})
+        }
+        console.log(validator.errors);
+        if(!validator.isEmpty()){
+            //El  validator.errors[0].msg muestra el primer error
+            return res.status(400).json({mensaje: validator.errors[0].msg})
+        }
+
         const usersToCreate = {
             id: users[users.length -1].id +1,
-            ...req.body
+            email:email,
+            password:bcrypt.hashSync(password, 10),
+            dni:dni,
+            tel:tel
         }
+        console.log();
         var usersToWrite = [...users,usersToCreate];
         fs.writeFileSync(usersFilePath, JSON.stringify(usersToWrite, null, 2));
         res.redirect('/');
@@ -30,22 +66,11 @@ const usersController = {
     },
 
     processLogin: (req, res) => {
-        let errors = validationResult(req);
-    
-        if (errors.isEmpty()) {
-            let users = getUsers();
-            let usuarioLogin;
-    
-            for(let i = 0; i < users.length; i++) {
-                if (users[i].email == req.body.email) {
-                    if (bcrypt.compareSync(req.body.password, users[i].password)) {
-                        usuarioLogin = users[i];
-                        break;
-                    }
-                }
-            }
-    
-            req.session.usuarioLogeado = usuarioLogin;
+        let users = getUsers();
+        let userLogin = users.find(user => user.email === req.body.email && bcrypt.compareSync(req.body.password, user.password));
+        if (userLogin) {
+            req.session.usuarioLogeado = userLogin;
+            console.log(req.session.usuarioLogeado);
             res.send('Logeado!');
         } else {
             return res.send('usuario invalido o contraseña incorrecta');
@@ -53,7 +78,6 @@ const usersController = {
     },
 
     userSave: (req, res) => {
-        console.log("llergaeg");
         const {email, password, confirmPassword, dni, tel } = req.body;
 
         if (!email) {
--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,22 @@ let router = express.Router();
 let usersController = require('../controllers/usersController.js');
 const { check } = require('express-validator');
 
+
+//
 router.get('/', usersController.login);
+router.post('/',usersController.processLogin);
 
-router.post('/',
-check('email').isEmail().withMessage('Email inválido.'),
-check('password').isLength({min: 8}).withMessage('La contraseña debe tener como mínimo 8 caracteres.'), 
-usersController.processLogin);
 
+//
 router.get ('/register', usersController.register);
-router.post('/register', usersController.create);
 
+router.post('/register',
+check('password').isLength({min: 8}).withMessage('La contraseña debe tener como mínimo 8 caracteres.'), 
+usersController.create);
+
+
+//
 router.post('/edit', usersController.userSave);
-
 router.post('/save', usersController.userSave);
 
 module.exports = router;
